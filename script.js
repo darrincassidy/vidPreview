b4aeeb1ab36d17d5c242d3b5306844b3072501d4
darrincassidy <darrincassidy@gmail.com>
2021-05-26 02:55:02 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+
+// I can only get the hover video to start/stop if I've click on the video atleast one time!
+
+function play(element) {
+    var x = document.getElementById("myVideo").duration;
+    x.play();
+  }
+  function pause(element) {
+    var x = document.getElementById("myVideo").duration;
+    x.pause();
+  }
